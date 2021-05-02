8939b576e20ad70451f0688b7eb38b6234b88cb5
timotheein <easer.ceo@gmail.com>
2021-05-02 20:47:59 +0200

    donno where the problem is from

--- ck.c
+++ ck.c
@@ -6,7 +6,7 @@
 /*   By: timotheein <timotheein@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 15:27:13 by timotheein        #+#    #+#             */
-/*   Updated: 2021/05/01 23:01:09 by timotheein       ###   ########.fr       */
+/*   Updated: 2021/05/02 20:43:40 by timotheein       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,31 @@ int main(int ac, char **av)
     all = 0;
     if (!(all = malloc(sizeof(t_all))))
         return (0);
+    write(1, "HEY5\n", 5);
+
     intitialize_all(all);
+    write(1, "HEY5\n", 5);
+
     if ((ac > 1) && !(ft_strcmp(av[1], "-v")))
         all->bonus = 1;
+    write(1, "HEY5\n", 5);
+
     if (ac < 2 || (ac < 3 && all->bonus))
         return (freeer(all));
+    write(1, "HEY5\n", 5);
+
     if (!check_n_get_arguments(ac, av, all))
         return (Error_arguments(all));
+    write(1, "HEY5\n", 5);
+
     if (!read_instruction(all))
         return (Error_instruction(all));
+    write(1, "HEY5\n", 5);
 
     if (!execute_instruction(all))
         return (Error_execution(all));
+    write(1, "HEY5\n", 5);
+
     all->check = check_final_order(all);
     check_result(all);
     return (freeer(all));
